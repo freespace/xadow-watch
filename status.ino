913c3505e06886fd11c1e03ba38696c8ce6e81e3
Shuning Bian <freespace@gmail.com>
2015-05-07 00:46:05 +0100

    Removes trailing space and updates use of BatLog in status

--- status.ino
+++ status.ino
@@ -5,35 +5,35 @@ void status_init() {
 
 void status_show() {
   uint8_t ypos = 0;
-  
-  dtostrf(Xadow.getBatVol(), 1, 1, _sbuf2); 
+
+  dtostrf(Xadow.getBatVol(), 1, 1, _sbuf2);
   sprintf(_sbuf, "Battery: %sV", _sbuf2);
   oled.drawString(_sbuf, 0, ypos, FONT_SIZE, STATUS_COLOR);
-  
-  ypos += LINE_SPACING;
+
+  ypos += LINE_HEIGHT;
 
   char *s;
   switch(Xadow.getChrgState()) {
       case NOCHARGE:
         s = "Not Charging";
         break;
-     
+
       case CHARGING:
         s = "Charging";
         break;
-       
+
       case CHARGDONE:
         s = "Done Charging";
         break;
-        
+
        default:
          s = "UNKNOWN";
   }
   oled.drawString(s, 0, ypos, FONT_SIZE, STATUS_COLOR);
 
-  ypos += LINE_SPACING;
+  ypos += LINE_HEIGHT;
 
-  if (logbat) s = "Logging Battery";
+  if (BatLog.logbat) s = "Logging Battery";
   else s = "Batt log exists";
 
   oled.drawString(s, 0, ypos, FONT_SIZE, STATUS_COLOR);
