e533373b7a088b38f13f3bf5bd3f132dca8cda16
Shuning Bian <freespace@gmail.com>
2015-05-07 00:46:28 +0100

    Update to use animation_boot and adds disconnect interaction during boot
    Disconnect interaction requires disconnection of USB before powering down USB
    module. Not doing so seems to be a source of kernel panics on OS X

--- xadow-watch.ino
+++ xadow-watch.ino
@@ -17,25 +17,22 @@
 
 #define LEFT_GUTTER_SIZE    (FONT_X)
 #define FONT_SIZE           (1)
-#define LINE_SPACING        (FONT_SIZE*FONT_Y+4)
+#define LINE_HEIGHT         (FONT_SIZE*FONT_Y+4)
 
-SSD1331 oled = SSD1331(cs, dc, mosi, sclk);  
+SSD1331 oled = SSD1331(cs, dc, mosi, sclk);
 
 char *test_menu[] = {"hello", "world", "here be", "dragons", NULL};
 
 char _sbuf[64];
 char _sbuf2[64];
 
-uint8_t logbat = 0;
-uint16_t eeaddr = 1;
-
 uint16_t now = 0;
 uint16_t checkpoint;
-
 
 typedef enum {
   SCREEN_WATCH,
   SCREEN_STATUS,
+  SCREEN_OTHER,
 } Screen;
 
 Screen currentScreen;
@@ -48,35 +45,31 @@ void setup() {
 
   oled.init();
   oled.fillScreen(COLOR_BLACK);
-  oled.drawString("BOOTING", (RGB_OLED_WIDTH-FONT_X*6)/2, RGB_OLED_HEIGHT/2-FONT_Y/2, 1, COLOR_BLUE);
-  delay(1000);
+  animation_boot();
   oled.fillScreen(COLOR_BLACK);
-  
+
   menu_init();
   status_init();
   watch_init(now);
-  
-  status_show();
-  
-  delay(1000);
-  
-  menu_show(test_menu);
-  
-  delay(1000);
-  
-  oled.fillScreen(COLOR_BLACK);
-  watch_show(0xFF);  
 
-  if (EEPROM[0] != 0x05) {
-    EEPROM[0] = 0x05;
-    logbat = 1;
-  } else {
-    Serial.begin(9600);
-    Serial.println("Battery log detected, not overwriting");
-    for (eeaddr = 1; eeaddr < 1024; ++eeaddr) {
-      Serial.println(EEPROM[eeaddr], DEC);
-    }
-  }
+  status_show();
+
+  delay(1000);
+
+  menu_show(test_menu);
+
+  delay(1000);
+
+  oled.fillScreen(COLOR_BLACK);
+
+  currentScreen = SCREEN_OTHER;
+
+  oled.drawString("Disconnect USB", 0, 0, FONT_SIZE, COLOR_RED);
+  oled.drawString("then press WAKE", 0, LINE_HEIGHT, FONT_SIZE, COLOR_RED);
+  while(digitalRead(10) == HIGH);;
+
+  now = 0;
+  checkpoint = millis();
 
   power_twi_disable();
   power_usart1_disable();
@@ -85,10 +78,6 @@ void setup() {
   power_timer1_disable();
   power_timer2_disable();
   power_timer3_disable();
-
-  now = 0;
-  checkpoint = millis();
-  currentScreen = SCREEN_WATCH;
 }
 
 void loop() {
@@ -100,6 +89,8 @@ void loop() {
 
   power_spi_enable();
   uint8_t changes = watch_tick(now);
+
+  batlog_tick(changes);
 
   Screen nextscreen = currentScreen;
 
@@ -122,15 +113,7 @@ void loop() {
 
     case SCREEN_WATCH:
       watch_show(changes);
-      break; 
-  }
-
-  if (changes >= 2 && logbat) {
-    if (eeaddr < 1024) {
-      uint8_t batvol = Xadow.getBatVol()*10;
-      EEPROM[eeaddr] = batvol;
-      eeaddr += 1;
-    }
+      break;
   }
 
   uint16_t t = millis();
