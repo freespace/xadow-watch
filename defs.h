59353625da6db43d6ef0b88f156ba509f2c47be6
Shuning Bian <freespace@gmail.com>
2015-05-30 01:51:36 +0100

    Adds new enum types

--- defs.h
+++ defs.h
@@ -1,3 +1,12 @@
 typedef uint32_t millis_t;
 typedef int32_t millis_delta_t;
 
+typedef enum {
+  BUZZER_SOUND_BUTTON,
+  BUZZER_SOUND_CHIME,
+} BuzzerSound;
+
+typedef enum {
+  BUTTON_WAKE,
+} ButtonName;
+
