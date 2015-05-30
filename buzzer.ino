1cc8eb1d76b2d3b1a6ae0f920f7cdf66b870f520
Shuning Bian <freespace@gmail.com>
2015-05-30 01:51:43 +0100

    Updates buzzer to use yo scale notes, updates API to be more flexible

--- buzzer.ino
+++ buzzer.ino
@@ -3,7 +3,14 @@
 #define BUZZER_FREQ_HZ                (4000)
 
 // standard duration of an alert
-#define BUZZER_DURATION_MS            (50)
+#define BUZZER_DURATION_MS            (100)
+
+#define D_NOTE                        (587)
+#define E_NOTE                        (659)
+#define G_NOTE                        (783)
+#define A_NOTE                        (880)
+#define B_NOTE                        (987)
+#define REST_NOTE                     (0)
 
 void buzzer_init() {
 }
@@ -12,46 +19,57 @@ void buzzer_init() {
  * Plays jingle number n
  */
 void buzzer_jingle(uint8_t n) {
-  buzzer_play(261, 100);
-  buzzer_play(293, 100);
-  buzzer_play(329, 100);
-  buzzer_play(349, 100);
-  buzzer_play(391, 100);
-  buzzer_play(440, 100);
+  uint8_t p = 100;
+  buzzer_play(D_NOTE, p);
+  buzzer_play(E_NOTE, p);
+  buzzer_play(G_NOTE, p);
+  buzzer_play(A_NOTE, p);
+  buzzer_play(B_NOTE, p);
 }
 
 /**
- * Sounds the buzzer at some default frequency for some default
- * duration
+ * Sounds the buzzer in response to buttons
  */
-void buzzer_sound() {
-  buzzer_play(440, BUZZER_DURATION_MS);
+void buzzer_sound(BuzzerSound sound) {
+  if (sound == BUZZER_SOUND_BUTTON) {
+    buzzer_play(E_NOTE, BUZZER_DURATION_MS);
+  } else if (sound == BUZZER_SOUND_CHIME) {
+    buzzer_play(B_NOTE, BUZZER_DURATION_MS);
+    buzzer_play(REST_NOTE, BUZZER_DURATION_MS);
+    buzzer_play(B_NOTE, BUZZER_DURATION_MS);
+  }
 }
+
+/**
+ * Sounds the 
 
 /**
  * Plays the tone specified by freq_hz for duration specified by dur_ms. Units
  * are in Hz and ms respectively.
  */ 
 void buzzer_play(uint16_t freq_hz, millis_delta_t dur_ms) {
-  // we need to end SPI b/c we use MISO to control
-  // the buzzer
-  SPI.end();
+  if (freq_hz == 0) delay(dur_ms);
+  else {
+    // we need to end SPI b/c we use MISO to control
+    // the buzzer
+    SPI.end();
 
-  // make MISO an output
-  DDRB |= _BV(PB3);
+    // make MISO an output
+    DDRB |= _BV(PB3);
 
-  millis_t now = millis();
+    millis_t now = millis();
 
-  // generate a 2 kHz tone using delay. Not elegant but close enough
-  uint16_t period_us = 1000000/freq_hz;
+    // generate a 2 kHz tone using delay. Not elegant but close enough
+    uint16_t period_us = 1000000/freq_hz;
 
-  while (millis() - now < dur_ms) {
-    PORTB |= _BV(PB3);
-    delayMicroseconds(period_us/2);
-    PORTB &= ~_BV(PB3);
-    delayMicroseconds(period_us/2);
+    while (millis() - now < dur_ms) {
+      PORTB |= _BV(PB3);
+      delayMicroseconds(period_us/2);
+      PORTB &= ~_BV(PB3);
+      delayMicroseconds(period_us/2);
+    }
+
+    // restart SPI or OLED will not work
+    SPI.begin();
   }
-
-  // restart SPI or OLED will not work
-  SPI.begin();
 }
