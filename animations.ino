0301df44f1c957281687bbc6249d2aa51abc59b1
Shuning Bian <freespace@gmail.com>
2015-05-30 01:52:09 +0100

    Shortens boot animation

--- animations.ino
+++ animations.ino
@@ -15,7 +15,7 @@ void animation_boot () {
     }
 
     oled.drawString(sequence[idx], (RGB_OLED_WIDTH-FONT_X*5)/2, RGB_OLED_HEIGHT/2-FONT_Y/2, 1, COLOR_BLUE);
-    delay(500);
+    delay(200);
   }
   delay(1000);
 }
