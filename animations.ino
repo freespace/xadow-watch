2aea8bd1d69a280f7ef0ed3407cece9221174efa
Shuning Bian <freespace@gmail.com>
2015-06-15 23:58:02 +0100

    Updates animation.ino to use new SSD1306 API

--- animations.ino
+++ animations.ino
@@ -9,18 +9,16 @@ void animations_boot () {
     NULL
   };
 
-  oled.setBufferRegion(0, 16, 128, 32);
+  oled.setBufferRegion(SSD1306_REGION_MID);
   uint8_t w = 128, h=32;
 
   for (uint8_t idx = 0; sequence[idx] != NULL; ++idx) {
     if (sequence[idx+1] == NULL) {
-      oled.drawString(sequence[idx-1], (w*5)/2, h/2-FONT_Y/2, 1, COLOR_BLACK);
-      oled.uploadBuffer();
+      oled.drawString(sequence[idx-1], (w-FONT_SPACE*6)/2, h/2-FONT_Y/2, 1, COLOR_BLACK);
     }
 
     oled.clearBuffer();
-    oled.drawString(sequence[idx], (w-FONT_X*5)/2, h/2-FONT_Y/2, 1, COLOR_BLUE);
-    oled.uploadBuffer();
+    oled.drawString(sequence[idx], (w-FONT_SPACE*6)/2, h/2-FONT_Y/2, 1, COLOR_BLUE);
     delay(200);
   }
   delay(1000);
